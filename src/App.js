7dfb53419dfc87c33f34c3a225b24014d7c81f10
Rastko Jokic <rale01jokic@gmail.com>
2024-02-02 20:37:26 +0100

    Created Home Page with usage of the custom hook to convert EUR to RSD, and its design using css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar></Navbar>
-     
+      <Routes>
+        <Route
+          path='/'
+          element={<Home/>}
+        />
+       
+      </Routes>
       </BrowserRouter>
       <Footer></Footer>
       
